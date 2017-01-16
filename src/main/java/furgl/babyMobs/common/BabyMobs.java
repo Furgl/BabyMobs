185664df8d07314a6b08f7f9c200c89bd7fc339e
Furgl <jake.pollard15@gmail.com>
2017-01-16 17:05:57 -0600

    v1.5.4

--- src/main/java/furgl/babyMobs/common/BabyMobs.java
+++ src/main/java/furgl/babyMobs/common/BabyMobs.java
@@ -37,7 +37,7 @@ public class BabyMobs
 {
 	public static final String MODID = "babymobs";
 	public static final String MODNAME = "Baby Mobs";
-	public static final String VERSION = "1.5.3";
+	public static final String VERSION = "1.5.4";
 	@Mod.Instance(MODID)
 	public static BabyMobs instance;
 	@SidedProxy(clientSide = "furgl.babyMobs.client.ClientProxy", serverSide = "furgl.babyMobs.common.CommonProxy")
